0fe9a7a224f09ae281c5eb9d753ab83cd363f634
Sam Ferree <sferree567@gmail.com>
2023-05-18 00:49:04 -0400

    Add System.Text.Json conversion library and Maybe converter

---
+++ LanguageExtensions.Text.Json/MaybeConverter.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace LanguageExtensions.Text.Json;
+
+public class MaybeConverterFactory : JsonConverterFactory
+{
+	public override bool CanConvert(Type typeToConvert)
+		=> typeToConvert.IsGenericType
+		&& typeToConvert.GetGenericTypeDefinition() == typeof(Maybe<>);
+
+	public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+	{
+		Type converterType = typeof(MaybeConverter<>).MakeGenericType(typeToConvert.GetGenericArguments()[0]);
+		return Activator.CreateInstance(converterType) as JsonConverter;
+	}
+}
+
+public class MaybeConverter<Data> : JsonConverter<Maybe<Data>>
+	where Data : notnull
+{
+	public override bool HandleNull => true;
+
+	public override Maybe<Data> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		object? result = JsonSerializer.Deserialize(ref reader, typeof(Data?));
+
+		return result is not null
+			? (Data)result
+			: No<Data>.Value;
+	}
+
+	public override void Write(Utf8JsonWriter writer, Maybe<Data> value, JsonSerializerOptions options)
+	{
+		if (value is Some<Data> someData)
+		{
+			JsonSerializer.Serialize(writer, someData.Value, someData.Value.GetType());
+		}
+		else
+		{
+			writer.WriteNullValue();
+		}
+	}
+}
---
+++ tests/LanguageExtensions.Test/Text.Json/MaybeConverterShould.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+using System.Text.Json;
+using LanguageExtensions.Text.Json;
+using Shouldly;
+
+namespace LanguageExtensions.Test.Text.Json;
+public class MaybeConverterShould
+{
+	public record ComplexObject(
+		Maybe<string> Name);
+
+	private readonly JsonSerializerOptions _options;
+
+	public MaybeConverterShould()
+	{
+		_options = new JsonSerializerOptions();
+		_options.Converters.Add(new MaybeConverterFactory());
+	}
+
+	[Fact]
+	public void SerializeSomeDataCorrectly()
+	{
+		string name = "John Doe";
+		ComplexObject complex = new(name);
+		string json = JsonSerializer.Serialize(complex, _options);
+		ComplexObject? deserialized = JsonSerializer.Deserialize<ComplexObject>(json, _options);
+
+		deserialized.ShouldNotBeNull()
+			.Name.ShouldBeOfType<Some<string>>()
+			.Value.ShouldBe(name);
+	}
+
+	[Fact]
+	public void SerializeNoDataCorrectly()
+	{
+		ComplexObject complex = new(No<string>.Value);
+		string json = JsonSerializer.Serialize(complex, _options);
+		ComplexObject? deserialized = JsonSerializer.Deserialize<ComplexObject>(json, _options);
+
+		_ = deserialized.ShouldNotBeNull()
+			.Name.ShouldBeOfType<No<string>>();
+	}
+}
